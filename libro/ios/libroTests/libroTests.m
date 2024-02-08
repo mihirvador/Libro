2c76404e07e2fe949d79562e7f678f8ebd208ca9
mihirvador <mihirrocks9999@gmail.com>
2024-02-07 19:24:36 -0500

    Intialized Working Stuff

--- libro/ios/libroTests/libroTests.m
+++ libro/ios/libroTests/libroTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface libroTests : XCTestCase
+@interface LibroTests : XCTestCase
 
 @end
 
-@implementation libroTests
+@implementation LibroTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
